8a40ccb7eb6dd26c858b69c2e955c50d7904050e
shadster <shakthisagarm5918@gmail.com>
2025-08-21 19:34:21 +0530

    doneee

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // Remove the basePath for custom domain
-  // basePath: '/landing-page',
-  // Remove the assetPrefix for custom domain
-  // assetPrefix: '/landing-page',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
