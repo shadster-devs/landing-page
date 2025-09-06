e78a96a654848d6ae2b6fb4c12ebf1f8e5cc35e1
shakthi-sagar <shakthisagarm5918@gmail.com>
2025-09-06 11:11:03 +0530

    done

--- src/components/ads/AdSenseAd.tsx
+++ src/components/ads/AdSenseAd.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, Ref } from 'react';
 import { ADS_CONFIG } from '@/constants/ads';
 
 interface AdSenseAdProps {
@@ -57,7 +57,7 @@ export default function AdSenseAd({
 
   return (
     <ins 
-      ref={adRef}
+      ref={adRef as Ref<HTMLModElement>}
       className={`adsbygoogle ${className} ${adLoaded ? 'ad-loaded' : 'ad-loading'}`}
       style={{
         display: 'block',
